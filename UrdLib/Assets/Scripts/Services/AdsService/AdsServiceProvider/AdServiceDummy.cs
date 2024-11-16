70053b3dcfb6c514aaa77de81284a4c82874f2c6
AntonioZu <antoniojesusnc@gmail.com>
2024-11-16 21:05:40 +0100

    generic ads

---
+++ UrdLib/Assets/Scripts/Services/AdsService/AdsServiceProvider/AdServiceDummy.cs
@@ -0,0 +1,48 @@
+using System;
+using Urd.Ads;
+using Urd.Services;
+
+namespace RubberDuck
+{
+    [Serializable]
+    public class AdServiceDummy : IAdsServiceProvider
+    {
+        public void Init(Action onInitCallback = null) {
+            
+        }
+
+        public void ShowBanner(AdsBannerModel adsBannerModel, Action<AdMobBannerError> onBannerLoaded)
+        {
+            var bannerError = new AdMobBannerError();
+            bannerError.SetAsError();
+            onBannerLoaded?.Invoke(bannerError);
+        }
+
+        public void HideBanner()
+        {
+        }
+
+        public void ShowInterstitial(Action<bool> onRewardVideoWatchedCallback)
+        {
+            onRewardVideoWatchedCallback?.Invoke(false);
+        }
+
+        public void HideInterstitial()
+        {
+        }
+
+        public void ShowRewardedVideo(Action<bool> onRewardVideoWatchedCallback)
+        {
+            onRewardVideoWatchedCallback?.Invoke(false);
+        }
+
+        public void HideRewardedVideo()
+        {
+        }
+
+        public bool CanShowRewardedVideo(bool loadIfCannot)
+        {
+            return false;
+        }
+    }
+}

5dcda4da6c9051c398ec68c81e08b39b4a834c47
AntonioZu <antoniojesusnc@gmail.com>
2024-11-22 01:52:57 +0100

    ui line renderer

--- UrdLib/Assets/Scripts/UI/UILineRenderer.cs
+++ UrdLib/Assets/Scripts/UI/UILineRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using MyBox;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,34 +8,48 @@ namespace Urd.UI
     [RequireComponent(typeof(Image))]
     public class UILineRenderer : MonoBehaviour
     {
-            [SerializeField]
-            private float _lineWidth = 10;
+        [SerializeField] private float _lineWidth = 10;
+        [SerializeField] private Transform _point1;
+        [SerializeField] private Transform _point2;
+        
+        private RectTransform _imageRectTransform;
 
-            [SerializeField]
-            private Vector2 _point1;
-            
-            [SerializeField]
-            private Vector2 _point2;
-            
-            public void SetPoints(Vector2 point1, Vector2 point2, float lineWidth = 10)
+        private void Awake()
+        {
+            _imageRectTransform = GetComponent<RectTransform>();
+        }
+
+        public void SetPoints(Transform point1, Transform point2, float lineWidth = 10)
+        {
+            _lineWidth = lineWidth;
+            _point1 = point1;
+            _point2 = point2;
+            Calculate();
+        }
+
+        void Update()
+        {
+            Calculate();
+        }
+        
+        [ButtonMethod]
+        private void Calculate()
+        {
+            if (_point1 == null || _point2 == null || _imageRectTransform == null)
             {
-                _lineWidth = lineWidth;
-                _point1 = point1;
-                _point2 = point2;
-                Calculate();
+                return;
             }
 
-            [ButtonMethod]
-            private void Calculate()
-            {
-                Vector3 differenceVector = _point2 - _point1;
-
-            var imageRectTransform = GetComponent<RectTransform>();
-            imageRectTransform.sizeDelta = new Vector2( differenceVector.magnitude, _lineWidth);
-            imageRectTransform.pivot = new Vector2(0, 0.5f);
-            imageRectTransform.position = _point1;
+            Vector2 finalPos01 = _point1.position.ToVector2() - Vector2.up * (100 * transform.lossyScale.x);
+            Vector2 finalPos02 = _point2.position.ToVector2() - Vector2.up * (100 * transform.lossyScale.x);
+            
+            Vector3 differenceVector = finalPos02 - finalPos01;
+            
+            _imageRectTransform.sizeDelta = new Vector2(differenceVector.magnitude, _lineWidth);
+            _imageRectTransform.pivot = new Vector2(0, 0.5f);
+            _imageRectTransform.position = finalPos01;
             float angle = Mathf.Atan2(differenceVector.y, differenceVector.x) * Mathf.Rad2Deg;
-            imageRectTransform.rotation = Quaternion.Euler(0,0, angle);
+            _imageRectTransform.rotation = Quaternion.Euler(0, 0, angle);
         }
     }
 }
